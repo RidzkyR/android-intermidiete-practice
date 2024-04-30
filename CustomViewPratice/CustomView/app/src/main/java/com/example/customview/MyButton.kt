7666697983a3a5eb860217471ec758d61bb14f28
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-30 11:31:27 +0700

    add onDraw function

--- CustomViewPratice/CustomView/app/src/main/java/com/example/customview/MyButton.kt
+++ CustomViewPratice/CustomView/app/src/main/java/com/example/customview/MyButton.kt
@@ -1,10 +1,15 @@
 package com.example.customview
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
 
 class MyButton: AppCompatButton {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
 }
