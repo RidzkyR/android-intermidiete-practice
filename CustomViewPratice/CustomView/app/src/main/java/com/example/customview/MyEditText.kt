7d3b4734ee8b80a73628e0667274fc738e3609b3
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-30 15:36:11 +0700

    add CustomView for edit text

---
+++ CustomViewPratice/CustomView/app/src/main/java/com/example/customview/MyEditText.kt
@@ -0,0 +1,11 @@
+package com.example.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+
+}
