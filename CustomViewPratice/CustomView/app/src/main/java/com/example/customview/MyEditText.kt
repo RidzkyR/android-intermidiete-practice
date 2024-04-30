fa9c8aa2513654cfb0a1dcda5451c0ffa6d88c6c
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-30 16:35:05 +0700

    add custom editText

--- CustomViewPratice/CustomView/app/src/main/java/com/example/customview/MyEditText.kt
+++ CustomViewPratice/CustomView/app/src/main/java/com/example/customview/MyEditText.kt
@@ -1,11 +1,114 @@
 package com.example.customview
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
 
 class MyEditText @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null
-) : AppCompatEditText(context, attrs) {
+    context: Context, attrs: AttributeSet? = null,
+) : AppCompatEditText(context, attrs), View.OnTouchListener {
+
+    private var clearButtonImage: Drawable
+
+    init {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Nothing
+            }
+
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Masukan Nama"
+        textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null,
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    // mengembalikan nilai false ketika editText di sentuh
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+
+            var isClearButtonClicked = false
+
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        showClearButton()
+                        return true
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
+    }
+
 
 }
