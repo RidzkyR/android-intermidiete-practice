2836ed4d69beb6955a1178da18663bb45840a3bd
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-02 01:24:52 +0700

    add face

--- Canvas-Pratice/app/src/main/java/com/example/likesapp/MainActivity.kt
+++ Canvas-Pratice/app/src/main/java/com/example/likesapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.likesapp
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.likesapp.databinding.ActivityMainBinding
@@ -17,6 +19,59 @@ class MainActivity : AppCompatActivity() {
     private val canvas = Canvas(bitmap)
     private val paint = Paint()
 
+    private val halfOfWidth = (bitmap.width / 2).toFloat()
+    private val halfOfHeight = (bitmap.height / 2).toFloat()
+
+    private fun showFace() {
+        val face = RectF(150F, 250F, bitmap.width - 105F, bitmap.height.toFloat() - 50F)
+
+        // menggambar kepala bagian kiri
+        paint.color =
+            ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null) // manggil warna
+        canvas.drawArc(face, 90F, 180F, false, paint)
+
+        // menggambar kepala bagian kanan
+        paint.color =
+            ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null) // manggil warna
+        canvas.drawArc(face, 270F, 180F, false, paint)
+    }
+
+    private fun showEye() {
+        // hati-hati penulisan baris kode berpengaruh terhadap hasil
+
+        // bola mata
+        paint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        canvas.drawCircle(halfOfWidth - 80F, halfOfHeight - 10F, 50F, paint)
+        canvas.drawCircle(halfOfWidth + 120F, halfOfHeight - 10F, 50F, paint)
+
+        // hightlight mata
+        paint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+        canvas.drawCircle(halfOfWidth - 105F, halfOfHeight - 20F, 15F, paint)
+        canvas.drawCircle(halfOfWidth + 100F, halfOfHeight - 20F, 15F, paint)
+    }
+
+    private fun showMouth(isHappy: Boolean) {
+        when (isHappy) {
+            true -> {
+                paint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+                val lip = RectF(halfOfWidth - 160F, halfOfHeight - 100F, halfOfWidth + 200F, halfOfHeight + 400F)
+                canvas.drawArc(lip, 25F, 130F, false, paint)
+
+                paint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+                val mouth = RectF(halfOfWidth - 140F, halfOfHeight, halfOfWidth + 180F, halfOfHeight + 380F)
+                canvas.drawArc(mouth, 25F, 130F, false, paint)
+            }
+            false ->{
+                paint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+                val lip = RectF(halfOfWidth - 160F, halfOfHeight + 250F, halfOfWidth + 200F, halfOfHeight + 350F)
+                canvas.drawArc(lip, 0F, -180F, false, paint)
+
+                paint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+                val mouth = RectF(halfOfWidth - 140F, halfOfHeight + 260F, halfOfWidth + 180F, halfOfHeight + 330F)
+                canvas.drawArc(mouth, 0F, -180F, false, paint)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +87,10 @@ class MainActivity : AppCompatActivity() {
 
         // inisialisai bitmap untuk canvas
         binding.imageView.setImageBitmap(bitmap)
+
+        showFace()
+        showEye()
+        showMouth(true)
 
     }
 }
