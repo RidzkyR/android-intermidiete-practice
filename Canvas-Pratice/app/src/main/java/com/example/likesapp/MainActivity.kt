129ca7457f056935735f78e4b7f8854d3450619d
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-02 00:44:51 +0700

    initialization canvas

--- Canvas-Pratice/app/src/main/java/com/example/likesapp/MainActivity.kt
+++ Canvas-Pratice/app/src/main/java/com/example/likesapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.likesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val bitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val canvas = Canvas(bitmap)
+    private val paint = Paint()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,6 +30,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // inisialisai bitmap untuk canvas
+        binding.imageView.setImageBitmap(bitmap)
 
     }
 }
