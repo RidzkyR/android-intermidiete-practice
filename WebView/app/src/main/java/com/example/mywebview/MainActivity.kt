9d5d7e4a980390a3c5a35d5507e46b4cfb8f7c95
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-05 01:20:50 +0700

    add google webview

--- WebView/app/src/main/java/com/example/mywebview/MainActivity.kt
+++ WebView/app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mywebview
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled", "WebViewApiAvailability")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.google.com/")
+
+        onBackPressedDispatcher.addCallback {
+            // Check whether there's history.
+            if (webView.canGoBack()) {
+                webView.goBack()
+            }
         }
     }
 }
