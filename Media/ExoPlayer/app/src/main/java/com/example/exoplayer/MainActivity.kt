d5c02908cf14bf26c81ef6a46d3d47406f1c54e3
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-10 06:31:20 +0700

    fix bug

--- Media/ExoPlayer/app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ Media/ExoPlayer/app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import com.example.exoplayer.databinding.ActivityMainBinding
@@ -24,15 +26,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        hideSystemUI()
+
         val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
         val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
 
         val exoPlayer = ExoPlayer.Builder(this).build().also {
             it.setMediaItem(videoItem)
-            it.setMediaItem(audioItem)
+            it.addMediaItem(audioItem)
             it.prepare()
         }
 
         binding.playerView.player = exoPlayer
+    }
+
+    private fun hideSystemUI() {
+        // mengatur fullscreen
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.playerView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
