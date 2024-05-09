fa1038fc468dc7294964427c31e08b08e25edb3f
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-09 16:30:07 +0700

    add foregroudService pratice

---
+++ Services/ServicePratice/app/src/main/java/com/example/myservice/ForegroundService.kt
@@ -0,0 +1,12 @@
+package com.example.myservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ForegroundService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
