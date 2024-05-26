f2a1ddfc63b15ec00c8819714c1b2012dba735f0
ridzky rahim <ridzkyrhm37@gmail.com>
2024-05-26 17:38:33 +0700

    add geofence

--- GeoLocation/Geofence/app/src/main/java/com/example/geofence/MapsActivity.kt
+++ GeoLocation/Geofence/app/src/main/java/com/example/geofence/MapsActivity.kt
@@ -11,6 +11,8 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.app.PendingIntent
+import android.content.Intent
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -18,34 +20,55 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.geofence.databinding.ActivityMapsBinding
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CircleOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var geofencingClient: GeofencingClient
+
     private val centerLat = 37.4274745
     private val centerLng = -122.169719
     private val geofenceRadius = 400.0
 
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+        intent.action = GeofenceBroadcastReceiver.ACTION_GEOFENCE_EVENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+    }
+
     private val requestNotificationPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) Toast.makeText(this@MapsActivity, "Notifications permission granted", Toast.LENGTH_SHORT).show()
-            else Toast.makeText(this@MapsActivity, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        if (Build.VERSION.SDK_INT >= 33) requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -53,24 +76,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.uiSettings.isZoomControlsEnabled = true
 
-        val stanfold = LatLng(centerLat, centerLng)
-        mMap.addMarker(MarkerOptions().position(stanfold).title("Stanfold University"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stanfold, 15f))
+        val stanford = LatLng(centerLat, centerLng)
+        mMap.addMarker(MarkerOptions().position(stanford).title("Stanford University"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stanford, 15f))
 
         mMap.addCircle(
             CircleOptions()
-                .center(stanfold)
+                .center(stanford)
                 .radius(geofenceRadius)
                 .fillColor(0x22FF0000)
                 .strokeColor(Color.RED)
                 .strokeWidth(3f)
         )
 
-        getLocation()
-    }
-
-    private fun getLocation(){
-
+        getMyLocation()
+        addGeofence()
     }
 
     private val requestBackgroundLocationPermissionLauncher =
@@ -83,6 +103,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
     private val runningQOrLater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
     @TargetApi(Build.VERSION_CODES.Q)
     private val requestLocationPermissionLauncher =
         registerForActivityResult(
@@ -96,12 +117,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+
     private fun checkPermission(permission: String): Boolean {
         return ContextCompat.checkSelfPermission(
             this,
             permission
         ) == PackageManager.PERMISSION_GRANTED
     }
+
     @TargetApi(Build.VERSION_CODES.Q)
     private fun checkForegroundAndBackgroundLocationPermission(): Boolean {
         val foregroundLocationApproved = checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -113,6 +136,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         return foregroundLocationApproved && backgroundPermissionApproved
     }
+
     @SuppressLint("MissingPermission")
     private fun getMyLocation() {
         if (checkForegroundAndBackgroundLocationPermission()) {
@@ -120,5 +144,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         } else {
             requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addGeofence() {
+        geofencingClient = LocationServices.getGeofencingClient(this)
+
+        val geofence = Geofence.Builder()
+            .setRequestId("kampus")
+            .setCircularRegion(
+                centerLat,
+                centerLng,
+                geofenceRadius.toFloat()
+            )
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_DWELL or Geofence.GEOFENCE_TRANSITION_ENTER)
+            .setLoiteringDelay(5000)
+            .build()
+
+        val geofencingRequest = GeofencingRequest.Builder()
+            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            .addGeofence(geofence)
+            .build()
+
+        geofencingClient.removeGeofences(geofencePendingIntent).run {
+            addOnCompleteListener {
+                geofencingClient.addGeofences(geofencingRequest, geofencePendingIntent).run {
+                    addOnSuccessListener {
+                        showToast("Geofencing added")
+                    }
+                    addOnFailureListener {
+                        showToast("Geofencing not added : ${it.message}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this@MapsActivity, text, Toast.LENGTH_SHORT).show()
     }
 }
